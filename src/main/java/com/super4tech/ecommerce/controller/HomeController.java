d34875b8d9e888d2d1d2c8fb0c0aa570b7763b57
Tsegazeab <tsegazeabgk@gmail.com>
2020-08-10 16:34:32 -0500

    controller addition

---
+++ src/main/java/com/super4tech/ecommerce/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.super4tech.ecommerce.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping
+    public String home(){
+        return "index";
+    }
+}
